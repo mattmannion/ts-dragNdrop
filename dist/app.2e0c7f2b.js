8dc00b7c74a649989ff842d1706aebe175374645
Matt Mannion <mattpmannion@gmail.com>
2021-01-25 10:58:37 -0500

    more ts added

--- dist/app.2e0c7f2b.js
+++ dist/app.2e0c7f2b.js
@@ -120,7 +120,76 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"../typescript/app.ts":[function(require,module,exports) {
 "use strict";
 
-console.log('test');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var __decorate = this && this.__decorate || function (decorators, target, key, desc) {
+  var c = arguments.length,
+      r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
+      d;
+  if ((typeof Reflect === "undefined" ? "undefined" : _typeof(Reflect)) === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);else for (var i = decorators.length - 1; i >= 0; i--) {
+    if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+  }
+  return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+function autobind(_, _2, descriptor) {
+  var originalMethod = descriptor.value;
+  var adjDescriptor = {
+    configurable: true,
+    get: function get() {
+      var boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+var ProjectInput = /*#__PURE__*/function () {
+  function ProjectInput() {
+    _classCallCheck(this, ProjectInput);
+
+    this.templateElement = document.getElementById('project-input');
+    this.hostElement = document.getElementById('app');
+    var importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild;
+    this.element.id = 'user-input';
+    this.titleInputElement = this.element.querySelector('#title');
+    this.descriptionInputElement = this.element.querySelector('#title');
+    this.peopleInputElement = this.element.querySelector('#title');
+    this.configure();
+    this.attach();
+  }
+
+  _createClass(ProjectInput, [{
+    key: "submitHandler",
+    value: function submitHandler(event) {
+      event.preventDefault();
+      console.log(this.titleInputElement.value);
+    }
+  }, {
+    key: "configure",
+    value: function configure() {
+      this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+  }, {
+    key: "attach",
+    value: function attach() {
+      this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+  }]);
+
+  return ProjectInput;
+}();
+
+__decorate([autobind], ProjectInput.prototype, "submitHandler", null);
+
+var prjInput = new ProjectInput();
 },{}],"../../../../../../../home/matt/.nvm/versions/node/v15.5.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -1,1 +1,65 @@
-console.log('test');
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: ProjectDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// Project Input class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById('project-input')!
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector('#title')
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector('#title')
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector('#title')
+    );
+
+    this.configure();
+    this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
